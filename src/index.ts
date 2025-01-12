0b3c88a52654671ae413410f9e8c8ac08b14f7aa
Jeong Seunghyeon <antony@kaist.ac.kr>
2025-01-12 07:07:49 +0000

    Initialize

---
+++ src/index.ts
@@ -0,0 +1,38 @@
+import { ApolloServer } from "@apollo/server";
+import { startStandaloneServer } from "@apollo/server/standalone";
+
+import { readFileSync } from "fs";
+import path from "path";
+import { gql } from "graphql-tag";
+
+const typeDefs = gql(
+  readFileSync(path.resolve(__dirname, "./schema.graphql"), {
+    encoding: "utf-8",
+  })
+)
+
+async function startApolloServer() {
+  const server = new ApolloServer({
+    typeDefs,
+  });
+  const { url } = await startStandaloneServer(
+    server,
+    {
+      context: async () => {
+        const { cache } = server;
+
+        return {
+          dataSources: {
+          }
+        }
+      },
+      listen: { port: 8080 }
+    }
+  );
+  console.log(`
+    🚀  Server is running!
+    📭  Query at ${url}
+  `);
+}
+
+startApolloServer();
