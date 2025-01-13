cd6f4551861be23743830140c84be960329c7afe
haram jeong <91401364+haramj@users.noreply.github.com>
2025-01-13 11:12:14 +0000

    code refactoring

--- src/resolvers/aladinAPIResolver.ts
+++ src/resolvers/aladinAPIResolver.ts
@@ -4,13 +4,9 @@ import { type Resolvers } from '../types/generated';
 export const resolvers: Resolvers = {
     Query: {
       searchBooks: async (_source: any, { query }: SearchText, { dataSources }) => {
-        // ListingAPI에서 책 검색
-        console.log("HI");
-        const searchText: SearchText = {query}
+        const searchText: SearchText = { query }
         const books = await dataSources.aladinAPI.searchBooks(searchText.query);
-
-        console.log(books);
-        return books; // 반환된 책 목록
+        return books;
       },
     },
   };
