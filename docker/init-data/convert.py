a4649eba2405083af079585995cc84825ce360a6
Jeong Seunghyeon <antony@kaist.ac.kr>
2025-02-20 14:44:19 +0000

    Escape slash characters in URL fields in test data

---
+++ docker/init-data/convert.py
@@ -0,0 +1,24 @@
+with open("booktemppre.csv", 'r', encoding='utf-8') as src:
+    with open("booktemp.csv", 'w', encoding='utf-8') as dest:
+        while True:
+            line = src.readline()
+            if len(line) == 0:
+                break
+            state = False
+            for c in line:
+                if c == '\\':
+                    if state:
+                        dest.write('\\')
+                        state = False
+                    else:
+                        state = True
+                elif c == '/':
+                    dest.write('/')
+                    state = False
+                else:
+                    if state:
+                        exit(-2)
+                    else:
+                        dest.write(c)
+            
+
