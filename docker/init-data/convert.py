454bc20587dfd9550bbac87a760b1c0e1d485758
Jeong Seunghyeon <antony@kaist.ac.kr>
2025-02-20 14:55:32 +0000

    Refactor docker/init-data/convert.py

--- docker/init-data/convert.py
+++ docker/init-data/convert.py
@@ -1,24 +1,26 @@
-with open("booktemppre.csv", 'r', encoding='utf-8') as src:
-    with open("booktemp.csv", 'w', encoding='utf-8') as dest:
-        while True:
-            line = src.readline()
-            if len(line) == 0:
-                break
-            state = False
-            for c in line:
-                if c == '\\':
-                    if state:
-                        dest.write('\\')
-                        state = False
-                    else:
-                        state = True
-                elif c == '/':
-                    dest.write('/')
+with \
+    open("booktemppre.csv", 'r', encoding='utf-8') as src, \
+    open("booktemp.csv", 'w', encoding='utf-8') as dest \
+:
+    while True:
+        line = src.readline()
+        if len(line) == 0:
+            break
+        state = False
+        for c in line:
+            if c == '\\':
+                if state:
+                    dest.write('\\')
                     state = False
                 else:
-                    if state:
-                        exit(-2)
-                    else:
-                        dest.write(c)
+                    state = True
+            elif c == '/':
+                dest.write('/')
+                state = False
+            else:
+                if state:
+                    exit(-2)
+                else:
+                    dest.write(c)
             
 
